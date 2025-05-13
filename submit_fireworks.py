85b6b9e5f3b11ab25ea01c28a148393c68159536
Nikita Kazeev <kazeevn@gmail.com>
2025-05-13 19:16:23 +0800

    FireWorks

---
+++ submit_fireworks.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+from argparse import ArgumentParser
+import pandas as pd
+from atomate2.vasp.flows.mp import MPGGADoubleRelaxStaticMaker
+from jobflow.managers.fireworks import flow_to_workflow
+from fireworks import LaunchPad
+from utils import load_cifs, to_structure
+
+SCRATCH_ROOT = Path.home().resolve() / "scratch" / "dft_runs"
+PROJECT_ROOT = Path(__file__).resolve().parent
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("structures", type=Path,
+                        help="CSV with structures")
+    parser.add_argument("run_name", type=str)
+    parser.add_argument("--sample-n", type=int, help="Only compute N randomly sampled structures")
+    parser.add_argument("--sampling-random-seed", type=int, default=42,
+                        help="Random seed for sampling only")
+    
+    args = parser.parse_args()
+    structures_str = load_cifs(args.structures)
+    if args.sample_n:
+        structures_str = structures_str.sample(args.sample_n)
+    
+    lpad = LaunchPad.auto_load()
+
+    def submit_structure(structure_id):
+        structure = to_structure(structures_str[structure_id])
+        flow = MPGGADoubleRelaxStaticMaker().make(structure=structure)
+        flow.update_metadata({
+            "material_id": structure_id,
+            "run_name": args.run_name,
+            "structure_source": args.structures})
+        wf = flow_to_workflow(flow)
+        lpad.add_wf(wf)
+
+    for structure_id in structures_str.index:
+        submit_structure(structure_id)
+
+
+if __name__ == "__main__":
+    main()
--- worker_local.py
+++ worker_local.py
@@ -2,26 +2,12 @@ from argparse import ArgumentParser
 from pathlib import Path
 import logging
 
-from pymatgen.core import Structure
-
 from atomate2.vasp.flows.mp import MPGGADoubleRelaxStaticMaker
 from jobflow import run_locally
 
-from utils import load_cifs
+from utils import load_cifs, to_structure
 
 logger = logging.getLogger(__file__)
-
-# https://github.com/facebookresearch/flowmm/blob/6a96aec3b6eba89f6fa07436f0c8837979abb285/src/flowmm/pymatgen_.py#L68
-def to_structure(structure: Structure | dict | str) -> Structure:
-    # CIF parser likes to print warnings
-    if isinstance(structure, dict):
-        return Structure.from_dict(structure)
-    elif isinstance(structure, str):
-        return Structure.from_str(structure, fmt="cif")
-    elif isinstance(structure, Structure):
-        return structure
-    else:
-        raise ValueError("Unknown structure format!")
 
 
 def main():
