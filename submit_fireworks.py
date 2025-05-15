7f7abffba99ee33114715ec7f014c80724bccb6d
Nikita Kazeev <kazeevn@gmail.com>
2025-05-16 01:53:32 +0800

    More file formats & continuation

--- submit_fireworks.py
+++ submit_fireworks.py
@@ -1,9 +1,12 @@
 from pathlib import Path
 from argparse import ArgumentParser
-from atomate2.vasp.flows.mp import MPGGADoubleRelaxStaticMaker
+from atomate2.vasp.flows.mp import MPGGADoubleRelaxStaticMaker, \
+    MPGGADoubleRelaxMaker, MPGGARelaxMaker, MPGGAStaticMaker
 from jobflow.managers.fireworks import flow_to_workflow
 from fireworks import LaunchPad
-from utils import load_cifs, to_structure
+from fireworks.fw_config import QUEUEADAPTER_LOC
+from fireworks.utilities.fw_serializers import load_object_from_file
+from utils import load_structures
 
 SCRATCH_ROOT = Path.home().resolve() / "scratch" / "dft_runs"
 PROJECT_ROOT = Path(__file__).resolve().parent
@@ -12,31 +15,46 @@ def main():
     parser = ArgumentParser()
     parser.add_argument("structures", type=Path,
                         help="CSV with structures")
+    parser.add_argument("--format", type=str, default="cif",
+                        help="structure format, passed to Structure.from_str")
     parser.add_argument("run_name", type=str)
     parser.add_argument("--sample-n", type=int, help="Only compute N randomly sampled structures")
     parser.add_argument("--sampling-random-seed", type=int, default=42,
                         help="Random seed for sampling only")
     
     args = parser.parse_args()
-    structures_str = load_cifs(args.structures)
+    structures = load_structures(args.structures, args.format)
     if args.sample_n:
-        structures_str = structures_str.sample(
+        structures = structures.sample(
             args.sample_n, random_state=args.sampling_random_seed)
     
     lpad = LaunchPad.auto_load()
+    print("Assuming that the current default FireWorks config will be used "
+          "to execute the job.")
+    walltime_str = load_object_from_file(QUEUEADAPTER_LOC).get("walltime")
+    h, m, s = map(int, walltime_str.split(":"))
+    walltime_seconds = h * 3600 + m * 60 + s
+    run_vasp_kwargs={"wall_time": walltime_seconds, "vasp_job_kwargs": {"auto_continue": True}}
 
-    def submit_structure(structure_id):
-        structure = to_structure(structures_str[structure_id])
-        flow = MPGGADoubleRelaxStaticMaker().make(structure=structure)
+    default_MPGGADoubleRelaxMaker = MPGGADoubleRelaxMaker()
+    double_realax_maker = MPGGADoubleRelaxMaker(
+        relax_maker1 = MPGGARelaxMaker(run_vasp_kwargs=run_vasp_kwargs),
+        relax_maker2 = MPGGARelaxMaker(run_vasp_kwargs=run_vasp_kwargs, 
+            copy_vasp_kwargs=default_MPGGADoubleRelaxMaker.relax_maker2.copy_vasp_kwargs))
+
+    default_MPGGADoubleRelaxStaticMaker = MPGGADoubleRelaxStaticMaker()
+    double_maker = MPGGADoubleRelaxStaticMaker(
+        relax_maker=double_realax_maker,
+        static_maker=MPGGAStaticMaker(run_vasp_kwargs=run_vasp_kwargs, 
+            copy_vasp_kwargs=default_MPGGADoubleRelaxStaticMaker.static_maker.copy_vasp_kwargs))
+
+    for structure_id, structure in structures.items():
+        flow = double_maker.make(structure=structure)
         flow.update_metadata({
             "material_id": structure_id,
             "run_name": args.run_name,
             "structure_source": args.structures})
-        wf = flow_to_workflow(flow)
-        lpad.add_wf(wf)
-
-    for structure_id in structures_str.index:
-        submit_structure(structure_id)
+        lpad.add_wf(flow_to_workflow(flow))
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -2,21 +2,21 @@ from pathlib import Path
 import pandas as pd
 from pymatgen.core import Structure
 
-def load_cifs(file: Path) -> pd.Series:
+def load_structures(file: Path, fmt: str) -> pd.Series:
     structures_df = pd.read_csv(file)
     if "material_id" in structures_df.columns:
         structures_df.set_index("material_id", inplace=True, append=False)
     else:
         structures_df.set_index(structures_df.columns[0], inplace=True, append=False)
-    return structures_df.cif
+    return structures_df.loc[:, fmt].transform(to_structure, fmt=fmt)
 
-# https://github.com/facebookresearch/flowmm/blob/6a96aec3b6eba89f6fa07436f0c8837979abb285/src/flowmm/pymatgen_.py#L68
-def to_structure(structure: Structure | dict | str) -> Structure:
+
+def to_structure(structure: Structure | dict | str, fmt: str = "cif") -> Structure:
     # CIF parser likes to print warnings
     if isinstance(structure, dict):
         return Structure.from_dict(structure)
     elif isinstance(structure, str):
-        return Structure.from_str(structure, fmt="cif")
+        return Structure.from_str(structure, fmt=fmt)
     elif isinstance(structure, Structure):
         return structure
     else:
